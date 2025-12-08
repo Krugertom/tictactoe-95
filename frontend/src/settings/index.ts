779e38bb3355954d26ddbd8056377d0801ccb44d
Krugertom <krugertom@outlook.com>
2025-12-07 23:46:44 -0500

    fix(fe): Fix base url settings

--- frontend/src/settings/index.ts
+++ frontend/src/settings/index.ts
@@ -2,5 +2,5 @@ import { createSettingsProxy } from 'envarna';
 import { ApiSettings } from './api';
 
 export const settings = createSettingsProxy({
-    app: () => ApiSettings.load(),
+    api: () => ApiSettings.load(),
 });
